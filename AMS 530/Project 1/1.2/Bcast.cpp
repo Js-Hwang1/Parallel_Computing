59f14ce8423859c4713a85289acaad728f58e65b
Js-Hwang1 <155587102+Js-Hwang1@users.noreply.github.com>
2024-09-29 19:06:10 -0400

    [Update] Bcast.cpp
    -Implemented a verification method

--- AMS 530/Project 1/1.2/Bcast.cpp
+++ AMS 530/Project 1/1.2/Bcast.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-
     // Setup MPI
     MPI_Init(&argc, &argv);
 
@@ -15,7 +14,7 @@ int main(int argc, char **argv) {
 
     int mynode;
     MPI_Comm_rank(MPI_COMM_WORLD, &mynode);
-
+    
     // Make data
     const int N = pow(10, 6);
     vector<float> data(N, 0.0); // Initialize array
@@ -23,26 +22,24 @@ int main(int argc, char **argv) {
     if (mynode == 0) {
         // Central vertex (C1) initializes the data
         for (int i = 0; i < N; i++) {
-            data[i] = static_cast<float>(i + 0.1234);  // Example data
+            data[i] = roundf((static_cast<float>(i) + 0.1234) * 10000) / 10000;  // Round to nearest tenth
         }
     }
 
-    // Make Graph 
     const int num_nodes = 16;
-
 
     int index[num_nodes] = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48};
 
     int edges[48] = {
-        1, 2, 3,    
-        0, 6, 10,     
-        0, 8, 12,       
-        0, 4, 14,       
-        3, 5, 9,           
+        1, 2, 3,
+        0, 6, 10,
+        0, 8, 12,
+        0, 4, 14,
+        3, 5, 9,
         4, 6, 11,
         1, 5, 7,
-        6, 8, 13,        
-        2, 7, 9,           
+        6, 8, 13,
+        2, 7, 9,
         4, 8, 15,
         1, 11, 15,
         5, 10, 12,
@@ -60,21 +57,47 @@ int main(int argc, char **argv) {
 
     vector<int> neighbors(num_neighbors);
     MPI_Graph_neighbors(graph_comm, mynode, num_neighbors, neighbors.data());
-
-    // Start timing
-    double start_time = MPI_Wtime();  
     
-    // Start the broadcast using Bcast
+    double start_time = MPI_Wtime();  
+
     MPI_Bcast(data.data(), N, MPI_FLOAT, 0, graph_comm);
 
-    // Stop time and print the time used
     double end_time = MPI_Wtime();
-    if (mynode == 0) { // Print only once
-        cout << "Broadcast completed in " << end_time - start_time << " seconds.\n";
+
+
+    MPI_Barrier(graph_comm); 
+
+    bool is_successful = true; 
+    for (int j = 0; j < N; j++) {
+        float expected_value = roundf((static_cast<float>(j) + 0.1234) * 10000) / 10000; 
+        if (fabs(data[j] - expected_value) > 0.00001) { 
+            cout << "Broadcast incomplete in node " << mynode << endl;
+            is_successful = false;  
+            break;  
+        }
     }
 
-    // Finalize MPI
+    int success_flag = is_successful ? 1 : 0;
+    if (mynode != 0) {
+        MPI_Send(&success_flag, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    } else {
+        vector<int> success_flags(totnodes, 1);  
+        for (int i = 1; i < totnodes; i++) {
+            MPI_Recv(&success_flags[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        bool all_successful = true;
+        for (int i = 0; i < totnodes; i++) {
+            if (success_flags[i] != 1) {
+                all_successful = false;
+                break;
+            }
+        }
+        if (all_successful) {
+            cout <<end_time - start_time<< endl;
+        }
+    }
     MPI_Finalize();
 
     return 0;
-} 
+}
